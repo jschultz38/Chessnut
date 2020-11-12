6fd966c8c2f2cdd5ea160f9c63083361f2a656e6
jschultz38 <jschultz38@gatech.edu>
2020-11-12 14:44:22 -0500

    Changed to be proper for this fork

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+#How to send that bitch over to pip: https://dzone.com/articles/executable-package-pip-install
 
 from setuptools import setup
 import os
@@ -8,14 +9,14 @@ if os.path.exists('README.txt'):
 
 # https://pythonhosted.org/setuptools/setuptools.html#id7
 setup(
-    name='Chessnut',
-    version='0.3.1',
+    name='Chessnut4jschultz38',
+    version='0.3.10',
     packages=['Chessnut'],
-    author="Chris Gearhart",
-    author_email="chris@gearley.com",
-    description="A basic chess model to imports/export FEN & finds moves.",
+    author="Jarrett S",
+    author_email="jschultz38@gatech.edu",
+    description="A basic chess model to imports/export FEN & finds moves. Made to fix an issue with parent repo.",
     long_description=long_description,
     license="UNLICENSE",
     keywords="chess",
-    url="https://github.com/cgearhart/Chessnut",
+    url="https://github.com/jschultz38/Chessnut",
 )
